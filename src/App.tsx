9eac2af34f0cb726ca24defdf35d2291fc86eee1
EgorZhuk <egorbug@gmail.com>
2022-12-03 18:45:33 +0300

    Preparation of the "Way of Samurai" project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Content from "./components/Content";
 
 function App() {
-    return (
-        <div>
-            <Header title={'This is my APP'}/>
-            <Content/>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div className="App">
+     Hello, samurai! Let's go!
+    </div>
+  );
 }
 
 export default App;
