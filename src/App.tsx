04434c789b9053cc4952e40e8e089eff7addabfb
EgorZhuk <egorbug@gmail.com>
2022-12-05 00:07:07 +0300

    lesson

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <Header/>
+        <Technologies/>
      Hello, samurai! Let's go!
     </div>
   );
+}
+
+function Header() {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News</a>
+            <a href="#">Feed</a>
+        </div>
+    )
+}
+function Technologies(){
+    return(
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
