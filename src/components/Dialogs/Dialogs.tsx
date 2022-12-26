99bb278949316b639e200bbaa9239ae7ce42b16e
EgorZhuk <egorbug@gmail.com>
2022-12-26 17:56:39 +0300

    work with Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -7,9 +7,22 @@ export const Dialogs = (props: PropsType) => {
   return (
 
     <div className={classes.contentContainer}>
-      Dialogs
+      <div className="dialogs">
+        <div className="dialogsItems">
+          <div className="dialog">One</div>
+          <div className="dialog">Two</div>
+          <div className="dialog">Three</div>
+          <div className="dialog">Four</div>
+          <div className="dialog">Five</div>
+        </div>
+        <div className="messages">
+          <div className="message">Hello</div>
+          <div className="message">How are you</div>
+          <div className="message">Yo</div>
+        </div>
+      </div>
     </div>
 
 
   );
-}
+};
