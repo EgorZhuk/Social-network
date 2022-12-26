92f5ff744ee443a4b97e888cd3b482f0cabd1fbb
EgorZhuk <egorbug@gmail.com>
2022-12-26 14:10:49 +0300

    add Dialogs component

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Dialogs.module.css';
+
+type PropsType = {}
+
+export const Dialogs = (props: PropsType) => {
+  return (
+
+    <div className={classes.contentContainer}>
+      Dialogs
+    </div>
+
+
+  );
+}
