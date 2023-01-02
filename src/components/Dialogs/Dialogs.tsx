08228b73ecc4771216b0d86fdee1d18d09f7adca
EgorZhuk <egorbug@gmail.com>
2023-01-02 09:00:55 +0300

    add styles for dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from 'react-router-dom';
 
 type PropsType = {}
 
 export const Dialogs = (props: PropsType) => {
   return (
-
-    <div className={classes.contentContainer}>
-      <div className="dialogs">
-        <div className="dialogsItems">
-          <div className="dialog">One</div>
-          <div className="dialog">Two</div>
-          <div className="dialog">Three</div>
-          <div className="dialog">Four</div>
-          <div className="dialog">Five</div>
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>
+        <div className={classes.dialog}>
+          <NavLink to="/dialogs/1" activeClassName={classes.active}>One</NavLink>
         </div>
-        <div className="messages">
-          <div className="message">Hello</div>
-          <div className="message">How are you</div>
-          <div className="message">Yo</div>
+        <div className={classes.dialog}>
+          <NavLink to="/dialogs/2" activeClassName={classes.active}>Two</NavLink>
+        </div>
+        <div className={classes.dialog}>
+          <NavLink to="/dialogs/3" activeClassName={classes.active}>Three</NavLink>
+        </div>
+        <div className={classes.dialog}>
+          <NavLink to="/dialogs/4" activeClassName={classes.active}>Four</NavLink>
+        </div>
+        <div className={classes.dialog}>
+          <NavLink to="/dialogs/5" activeClassName={classes.active}>Five</NavLink>
         </div>
       </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hello</div>
+        <div className={classes.message}>How are you</div>
+        <div className={classes.message}>Yo</div>
+      </div>
     </div>
-
-
   );
 };
