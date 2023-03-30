638d4441c93bf4482f3007660a7ca506d36d8c72
EgorZhuk <egorbug@gmail.com>
2023-03-30 11:37:47 +0300

    refactoring Users

--- src/components/Users/User.tsx
+++ src/components/Users/User.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import s from './User.module.css';
-import {usersType} from '../../redux/users-reducer';
 
 type PropsType = {
-  users: usersType[]
-  follow: (userId: number) => void
-  unFollow: (userId: number) => void
-  setUsers: () => void
+  id: number,
+  photoUrl: string
+  follow: boolean
+  fullName: string,
+  status: string,
+  location: {
+    city: string,
+    country: string
+  }
+  followCalback: (userId: number) => void
+  unFollowCalback: (userId: number) => void
+
 }
 const User = (props: PropsType) => {
-  const showMoreHandler = () => {
-    props.setUsers();
-  };
-
-
-  let usersForRender = props.users.map((el) =>
-    <div className={s.userCard}>
+  return (
+    <div key={props.id} className={s.userCard}>
       <div className={s.userImage}>
         <img
-          src={el.photoUrl}
+          src={props.photoUrl}
           alt="avatar"/>
-        {el.follow
+        {props.follow
           ? <button onClick={() => {
-            props.unFollow(el.id);
+            props.unFollowCalback(props.id);
           }}>Unfollow</button>
           : <button onClick={() => {
-            props.follow(el.id);
+            props.followCalback(props.id);
           }}>Follow</button>
         }
 
@@ -33,26 +35,19 @@ const User = (props: PropsType) => {
 
       <div className={s.userInfo}>
         <div className={s.userTitle}>
-          <p className={s.userName}>{el.fullName}</p>
-          <p className={s.userStatus}>{el.status}</p>
+          <p className={s.userName}>{props.fullName}</p>
+          <p className={s.userStatus}>{props.status}</p>
         </div>
         <div className={s.userLocation}>
-          <p>{el.location.country},</p>
-          <p>{el.location.city}</p>
+          <p>{props.location.country},</p>
+          <p>{props.location.city}</p>
         </div>
       </div>
 
     </div>
   );
 
-  return (<>
-      <div className={s.userContainer}>
-        {usersForRender}
-        <button onClick={showMoreHandler}>SHOW MORE</button>
-      </div>
 
-    </>
-  );
 };
 
 export default User;
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {UsersPropsType} from './UsersContainer';
 import User from './User';
+import s from './Users.module.css';
 
 const Users = (props: UsersPropsType) => {
   const showMoreHandler = () => {
@@ -12,15 +13,28 @@ const Users = (props: UsersPropsType) => {
   const unFollowHandler = (userId: number) => {
     props.unFollow(userId);
   };
-  return (
-    <div>
-      <User
-        users={props.users}
-        follow={followHandler}
-        unFollow={unFollowHandler}
-        setUsers={showMoreHandler}
-      />
 
+  return (
+    <div className={s.userContainer}>
+      {
+        props.users.map((u, index) => {
+          return (
+            <User id={u.id}
+                  key={index}
+                  photoUrl={u.photoUrl}
+                  follow={u.follow}
+                  fullName={u.fullName}
+                  status={u.status}
+                  location={u.location}
+                  followCalback={followHandler}
+                  unFollowCalback={unFollowHandler}/>
+
+          );
+        })
+      }
+      <div>
+        <button onClick={showMoreHandler}>Show more</button>
+      </div>
     </div>
   );
 };
